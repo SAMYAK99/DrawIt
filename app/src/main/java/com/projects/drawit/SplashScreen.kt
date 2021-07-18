a01ff96fc8757ee5f752fdf99d55377108dcfc45
SAMYAK99 <singhsamyak199@gmail.com>
2021-07-18 22:51:34 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/projects/drawit/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.projects.drawit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+         Handler().postDelayed({
+             val i =Intent(this ,MainActivity::class.java)
+            startActivity(i)
+             finish()
+         },3000)
+    }
+}
