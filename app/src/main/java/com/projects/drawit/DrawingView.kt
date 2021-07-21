a4998335d41e540fb0455a3f53699c40c058ee9c
SAMYAK99 <singhsamyak199@gmail.com>
2021-07-21 23:12:02 +0530

    Dwawing View

---
+++ app/src/main/java/com/projects/drawit/DrawingView.kt
@@ -0,0 +1,10 @@
+package com.projects.drawit
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+class DrawingView (context: Context , attributes: AttributeSet) : View (context , attributes) {
+    
+}
