507a42daeae278f318eea6dcd21be79f721684ae
SAMYAK99 <singhsamyak199@gmail.com>
2021-07-27 01:09:59 +0530

    Adding the image from Local Storage

--- app/src/main/java/com/projects/drawit/MainActivity.kt
+++ app/src/main/java/com/projects/drawit/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.projects.drawit
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 import petrov.kristiyan.colorpicker.ColorPicker
@@ -16,13 +24,31 @@ class MainActivity : AppCompatActivity() {
 
         drawing_view.setSizeForBrush(20.toFloat()) // Setting the default brush size to drawing view.
 
-      // Selecting the Brush Size
+        // Selecting the Brush Size
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
 
-        colourPicker.setOnClickListener{
-           selectColourDialog()
+        colourPicker.setOnClickListener {
+            selectColourDialog()
+        }
+
+        // Setting the background image
+        gallery.setOnClickListener{
+            if(isReadStorageAllowed()) {
+                // run our code
+                if (isReadStorageAllowed()) {
+                    // This is for selecting the image from local store or let say from Gallery/Photos.
+                    val pickPhoto = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                    startActivityForResult(pickPhoto, GALLERY)
+                }
+            }
+            else{
+                request_storage_permission()
+            }
         }
     }
 
@@ -74,6 +100,87 @@ class MainActivity : AppCompatActivity() {
 
 
         brushDialog.show()
+    }
+
+
+    // getting the image from storage
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                try {
+                    if (data!!.data != null) {
+                        // Here if the user selects the image from local storage make the image view visible.
+                        // By Default we will make it VISIBILITY as GONE.
+                        iv_background.visibility = View.VISIBLE
+                        // Set the selected image to the backgroung view.
+                        iv_background.setImageURI(data.data)
+                    } else {
+                        // If the selected image is not valid. Or not selected.
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Error in parsing the image or its corrupted.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+
+
+
+    private fun request_storage_permission() {
+        // Informing the user if it denies
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).toString())) {
+            Toast.makeText(this, "Need Permission to add Image is Screen", Toast.LENGTH_SHORT)
+                .show()
+        }
+        // Requesting the permission to user
+        ActivityCompat.requestPermissions( this, arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ), STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode== STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty()&&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Now you can read Storage Files", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else{
+                Toast.makeText(this, "Please allow the permission to read files", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+
+
+     // We are calling this method to check the permission status
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 
 }
